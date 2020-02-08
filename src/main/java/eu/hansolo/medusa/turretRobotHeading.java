53f9fb11955729e03109edfbe3c7602db74dad24
Stephen-Schuster <46509946+Stephen-Schuster@users.noreply.github.com>
2020-02-07 20:05:39 -0800

    making basic skeleton

---
+++ src/main/java/eu/hansolo/medusa/turretRobotHeading.java
@@ -0,0 +1,29 @@
+package eu.hansolo.medusa;
+
+
+import java.time.Duration;
+import java.time.Instant;
+import java.time.LocalTime;
+import java.time.ZoneId;
+import java.time.ZonedDateTime;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.List;
+import java.util.Locale;
+import java.util.concurrent.CopyOnWriteArrayList;
+import java.util.concurrent.ScheduledExecutorService;
+import java.util.concurrent.ScheduledFuture;
+import java.util.concurrent.ScheduledThreadPoolExecutor;
+import java.util.concurrent.ThreadFactory;
+import java.util.concurrent.TimeUnit;
+
+
+/**
+ * Created by hansolo on 28.01.16.
+ */
+public class turretRobotHeading extends Control {
+    float robotHeading;
+    float turretHeading;
+    
+}
